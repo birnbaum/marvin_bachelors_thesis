fe3abe24522cd1ef09c63e3423358d8418f557c6
marvin-steinke <marvin.steinke@mail.de>
2022-07-22 15:21:49 +0200

    cleanup and f strings

--- src/SDL_Pi_status.py
+++ src/SDL_Pi_status.py
@@ -1,25 +1,24 @@
-import SDL_Pi_SunControl
+import SDL_Pi_SunControl as sdl
 
-# config
-sunControl = SDL_Pi_SunControl.SDL_Pi_SunControl(
-        INA3221Address = 0x40,
-        USBControlEnable = 26,
-        USBControlControl = 21,
-        WatchDog_Done = 13,
-        WatchDog_Wake = 16
+sc = sdl.SDL_Pi_SunControl(
+    INA3221Address = 0x40,
+    USBControlEnable = 26,
+    USBControlControl = 21,
+    WatchDog_Done = 13,
+    WatchDog_Wake = 16
 )
 
-label = 'LIPO_Battery'
-print('%s Load Voltage :\t  %3.2f V' % (label, sunControl.readChannelVoltageV(SDL_Pi_SunControl.SunControl_LIPO_BATTERY_CHANNEL)))
-print('%s Current :\t\t  %3.2f mA' % (label, sunControl.readChannelCurrentmA(SDL_Pi_SunControl.SunControl_LIPO_BATTERY_CHANNEL)))
-print()
+print('SDL_Pi_SunControl Power Status\n')
 
-label = 'Solar Cell'
-print('%s Load Voltage :\t  %3.2f V' % (label, sunControl.readChannelVoltageV(SDL_Pi_SunControl.SunControl_SOLAR_CELL_CHANNEL)))
-print('%s Current :\t\t  %3.2f mA' % (label, sunControl.readChannelCurrentmA(SDL_Pi_SunControl.SunControl_SOLAR_CELL_CHANNEL)))
-print()
+batteryVoltage = sc.readChannelVoltageV(sdl.SunControl_LIPO_BATTERY_CHANNEL)
+batteryCurrent = sc.readChannelCurrentmA(sdl.SunControl_LIPO_BATTERY_CHANNEL)
+print(f'LiPo Battery: \t {batteryVoltage:3.2f} V / {batteryCurrent:3.2f} mA')
+# TODO Battery SoC
 
-label = 'Output'
-print('%s Load Voltage :\t\t  %3.2f V' % (label, sunControl.readChannelVoltageV(SDL_Pi_SunControl.SunControl_OUTPUT_CHANNEL)))
-print('%s Current :\t\t  %3.2f mA' % (label, sunControl.readChannelCurrentmA(SDL_Pi_SunControl.SunControl_OUTPUT_CHANNEL)))
-print()
+solarCellVoltage = sc.readChannelVoltageV(sdl.SunControl_SOLAR_CELL_CHANNEL)
+solarCellCurrent = sc.readChannelCurrentmA(sdl.SunControl_SOLAR_CELL_CHANNEL)
+print(f'Solar Cells: \t {solarCellVoltage:3.2f} V / {solarCellCurrent:3.2f} mA')
+
+piVoltage = sc.readChannelVoltageV(sdl.SunControl_OUTPUT_CHANNEL)
+piCurrent = sc.readChannelCurrentmA(sdl.SunControl_OUTPUT_CHANNEL)
+print(f'Pi: \t {piVoltage:3.2f} V / {piCurrent:3.2f} mA')
