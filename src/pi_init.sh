26510655c70ad5f783f99db08ba9c09b5ccf2af9
marvin-steinke <marvin.steinke@mail.de>
2022-08-13 14:52:56 +0200

    replaced symlinks with real copies

--- src/pi_init.sh
+++ src/pi_init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ln -sf ./pi_rc.local /etc/rc.local                   # symlink to startup script
-ln -sf ./pi_config.txt /boot/config.txt              # symlink to pi_config.txt
+cp ./pi_rc.local /etc/rc.local
+cp ./pi_config.txt /boot/config.txt
 echo '1-1' | tee /sys/bus/usb/drivers/usb/unbind    # disable usb
 tvservice -o                                        # disable hdmi
