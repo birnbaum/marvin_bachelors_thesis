f061a064cfe0cd8de587667ea2e66ab57599d986
marvin-steinke <marvin.steinke@mail.de>
2022-08-14 13:13:20 +0200

    put startup scripts in rc.local, init script only runs once

--- src/pi_init.sh
+++ src/pi_init.sh
@@ -2,5 +2,3 @@
 
 cp ./pi_rc.local /etc/rc.local
 cp ./pi_config.txt /boot/config.txt
-echo '1-1' | tee /sys/bus/usb/drivers/usb/unbind    # disable usb
-tvservice -o                                        # disable hdmi
