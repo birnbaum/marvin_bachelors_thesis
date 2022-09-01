be7f7565946353d6d0dd8e5814f24c1b37c25c8e
marvin-steinke <marvin.steinke@mail.de>
2022-09-01 13:11:38 +0200

    fixed path

--- src/init.sh
+++ src/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-chmod 755 .config/rc.local
-chmod 755 .config/config.txt
-cp .config/rc.local /etc/rc.local
-cp .config/config.txt /boot/config.txt
+chmod 755 ./config/rc.local
+chmod 755 ./config/config.txt
+cp ./config/rc.local /etc/rc.local
+cp ./config/config.txt /boot/config.txt
