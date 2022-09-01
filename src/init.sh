66734d55d7ab029d563475586d82e3ba8a76e57d
marvin-steinke <marvin.steinke@mail.de>
2022-09-01 12:58:06 +0200

    adjustments to new file structure

--- src/init.sh
+++ src/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-chmod 755 ./pi_rc.local
-chmod 755 ./pi_config.txt
-cp ./pi_rc.local /etc/rc.local
-cp ./pi_config.txt /boot/config.txt
+chmod 755 .config/rc.local
+chmod 755 .config/config.txt
+cp .config/rc.local /etc/rc.local
+cp .config/config.txt /boot/config.txt
