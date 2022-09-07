35b61eb872a4e251952025452867aa6e6660d9f1
marvin-steinke <marvin.steinke@mail.de>
2022-09-07 09:53:08 +0200

    added script to gather solar data

---
+++ src/solar_data.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import time
+import statistics
+from lib import SDL_Pi_SunControl as sdl
+
+# config
+sc = sdl.SDL_Pi_SunControl(
+        INA3221Address = 0x40,
+        USBControlEnable = 26,
+        USBControlControl = 21,
+        WatchDog_Done = 13,
+        WatchDog_Wake = 16
+)
+
+time.sleep(10)
+starttime = time.time()
+# get every second idle current for 10 minutes
+currents = []
+while cputime := time.time() - starttime < 600:
+    currents.append(sc.readChannelCurrentmA(sdl.SunControl_SOLAR_CELL_CHANNEL))
+    time.sleep(1)
+# append currents and mean to file
+with open('solar_currents', 'a') as file:
+    for current in currents:
+        file.write(f'{current:3.2f}\n')
+    file.write(f'\nmean: {statistics.mean(currents)}')
