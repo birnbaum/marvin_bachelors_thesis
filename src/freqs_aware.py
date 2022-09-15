b45692567624bfb39f22ae806785a95608bf8885
marvin-steinke <marvin.steinke@mail.de>
2022-09-15 16:54:16 +0200

    new approximation method

--- src/freqs_aware.py
+++ src/freqs_aware.py
@@ -21,76 +21,49 @@ def tex_plot(output_file, data, legendentry):
 def pi_current(sc):
     return int(sc.readChannelCurrentmA(sdl.SunControl_OUTPUT_CHANNEL))
 
-def afterwatch(cpu, sc, should_current, stop):
-    currents = []
-    for _ in range(0, 5):
-        currents.append(pi_current(sc))
-        if stop():
-            return
-        sleep(1)
-    mean = statistics.mean(currents)
-    freqs = cpu.available_frequencies
-    freq_index = freqs.index(cpu.get_frequencies()[0])
-    if mean > should_current:
-        cpu.set_max_frequencies(freqs[freq_index - 1])
-    elif mean < should_current:
-        cpu.set_max_frequencies(freqs[freq_index + 1])
-    else:
-        return
-
-def log_search(cpu, sc, should_current, stop):
+def log_part_search(cpu, sc, should_current, L, R):
     is_current = pi_current(sc)
     freqs = cpu.available_frequencies
     freq_index = freqs.index(cpu.get_frequencies()[0])
-    if is_current < should_current:
-        L = freq_index
-        R = len(freqs) - 1
-    elif is_current > should_current:
-        L = 0
-        R = freq_index
-    else:
-        return
-    while abs(L - R) > 2:
-        M = math.ceil((L + R) / 2)
-        cpu.set_max_frequencies(freqs[M])
-        sleep(5)
-        is_current = pi_current(sc)
+    if not (L[1] < should_current < R[1]):
         if is_current < should_current:
-            L = M + 1
+            L[0] = freq_index
+            L[1] = is_current
+            R[0] = len(freqs) - 1
         elif is_current > should_current:
-            R = M - 1
+            L[0] = 0
+            R[0] = freq_index
+            R[1] = is_current
         else:
-            break
-        if stop():
-            return
-    afterwatch(cpu, sc, should_current, lambda: stop)
+            return L, R
+    M = math.floor((L + R) / 2)
+    cpu.set_max_frequencies(freqs[M])
+    sleep(5)
+    is_current = pi_current(sc)
+    if is_current < should_current:
+        L[0] = M + 1
+        L[1] = is_current
+    elif is_current > should_current:
+        R[0]= M - 1
+        R[1] = is_current
+    return L, R
 
 def aware(cpu, sc, input_file, output_file):
     solar_currents = read_file(input_file)
-    tex_plot(output_file, solar_currents, 'Solar Currents')
+    #tex_plot(output_file, solar_currents, 'Solar Currents')
     solar_currents.reverse()
     pi_currents = []
     pi_freqs = []
     window = []
-    search = Thread()
-    stop_search = False
-    while True:
+    L = (0, 0)
+    R = (0, 0)
+    while solar_currents:
         window.append(solar_currents.pop())
         pi_currents.append(pi_current(sc))
         pi_freqs.append(cpu.get_frequencies()[0])
         if len(window) == 5:
-            if search.is_alive():
-                stop_search = True
-                search.join()
-                stop_search = False
-            search = Thread(
-                        target=log_search,
-                        args=(cpu, sc, statistics.mean(window), lambda: stop_search)
-                     )
-            search.start()
+            L, R = log_part_search(cpu, sc, statistics.mean(window), L, R)
             window = []
-        if not solar_currents:
-            break
         sleep(1)
     tex_plot(output_file, pi_currents, 'Pi Currents')
     tex_plot(output_file, pi_freqs, 'Pi Frequencies')
